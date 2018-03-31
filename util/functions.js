e50a069d6e1fdc93f0593c024ae3a90f249cddf7
sujayvenaik <sujayvenaik@gmail.com>
2018-03-31 18:35:49 +0530

    added functions for api calls

--- util/functions.js
+++ util/functions.js
@@ -1,26 +1,9 @@
-var request = require("request");
+var request = require("request"),
+  _ = require('lodash');
 
 module.exports = {
-  authenticate : function () {
 
-
-    var options = { method: 'GET',
-      url: 'http://api.wordnik.com:80/v4/account.json/authenticate/sujayvenaik@gmail.com',
-      qs: 
-      { password: 'Sujay2396#',
-        api_key: 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5' },
-      headers: 
-      { 
-        password: 'Sujay2396#',
-        username: 'sujayvenaik@gmail.com' } };
-
-    request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-      console.log(body);
-    });
-  },
-
-  wordDefinition : function  (word) {
+  wordDefinition : function  (word, cb) {
     var options = { method: 'GET',
       url: `http://api.wordnik.com:80/v4/word.json/${word}/definitions`,
       qs: 
@@ -33,9 +16,74 @@ module.exports = {
 
     request(options, function (error, response, body) {
       if (error) throw new Error(error);
-
       console.log(`The def for word: ${word}\n${JSON.parse(body)[0].text}`);
+      cb(null, `The def for word: ${word}\n${JSON.parse(body)[0].text}\n`);
     });
 
+  },
+
+  wordSynonym : function (word, cb) {
+    var options = { method: 'GET',
+      url: `http://api.wordnik.com:80/v4/word.json/${word}/relatedWords`,
+      qs: 
+      { useCanonical: 'false',
+        relationshipTypes: 'synonym',
+        limitPerRelationshipType: '10',
+        api_key: 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5' },
+      headers:{ } };
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+      let snippet = `The Synonyms for the word ${word} are:\n`;
+      _.forEach(JSON.parse(body)[0].words, function(value) {
+        snippet += value + '\n';
+      });
+      console.log(snippet);
+      cb(null, snippet);
+    });
+
+  },
+  wordAntonym : function (word, cb) {
+    var options = { method: 'GET',
+      url: `http://api.wordnik.com:80/v4/word.json/${word}/relatedWords`,
+      qs: 
+      { useCanonical: 'false',
+        relationshipTypes: 'antonym',
+        limitPerRelationshipType: '10',
+        api_key: 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5' },
+      headers:{ } };
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+      let snippet = `The Antonyms for the word ${word} are:\n`;
+      _.forEach(JSON.parse(body)[0].words, function(value) {
+        snippet += value + '\n';
+      });
+      console.log(snippet);
+      cb(null, snippet);
+    });
+
+  },
+  wordExamples : function (word, cb) {
+    var options = { method: 'GET',
+      url: 'http://api.wordnik.com:80/v4/word.json/hello/examples',
+      qs: 
+      { includeDuplicates: 'false',
+        useCanonical: 'false',
+        skip: '0',
+        limit: '5',
+        api_key: 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5' },
+      headers: {} };
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+      let res = JSON.parse(body);
+      let snippet = `The Examples for the word ${word} are:\n`;
+      _.forEach(res.examples, function (value) {
+        snippet += value.text + '\n';
+      });
+    console.log(snippet);
+    cb(null, snippet);
+  });
   }
 }

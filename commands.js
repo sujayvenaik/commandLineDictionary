0780c23b3ffb7d8b1c966e27ac48fe3dd9629afc
sujayvenaik <sujayvenaik@gmail.com>
2018-03-31 18:36:02 +0530

    added other commands

--- commands.js
+++ commands.js
@@ -1,7 +1,30 @@
 #!/usr/bin/env node
 var program = require('commander'),
+  async = require('async'),
   authenticate = require('./util/functions').authenticate,
-  wordDef = require('./util/functions').wordDefinition;
+  wordDef = require('./util/functions').wordDefinition,
+  wordSynonym = require('./util/functions').wordSynonym,
+  wordAntonym = require('./util/functions').wordAntonym,
+  wordExamples = require('./util/functions').wordExamples;
+
+function wordAllDetails (word) {
+  async.series({
+    one: function(callback) {
+      wordDef(word, function (err, snippet) {callback(null, 1);});
+    },
+    two: function(callback){
+        wordSynonym(word, function (err, snippet) { callback(null, 2)});
+    },
+    three: function (callback) {
+      wordAntonym(word, function (err, snippet) { callback(null, 3)});
+    },
+    four: function (callback) {
+      wordExamples(word, function (err, snippet) { callback (null, 4)});
+    }
+  }, function(err, results) {
+      if(err) { console.log('Something Wrong'); }
+  });
+};
 
 program
   .version('0.0.1')
@@ -15,6 +38,24 @@ program
   .version('0.0.1')
   .command('def <word>')
   .description('Definition for a word')
-  .action(word => wordDef(word));
+  .action(word => wordDef(word, function (err,snippet) {}));
+
+program
+  .version('0.0.1')
+  .command('syn <word>')
+  .description('Synonyms for a word')
+  .action(word => wordSynonym(word, function (err,snippet) {}));
+
+program
+  .version('0.0.1')
+  .command('ant <word>')
+  .description('Antonyms for a word')
+  .action(word => wordAntonym(word, function (err,snippet) {}));
+
+program
+  .version('0.0.1')
+  .command('dict <word>')
+  .description('Examples for a word')
+  .action(word => wordAllDetails(word, function (err,snippet) {}));
 
 program.parse(process.argv);
